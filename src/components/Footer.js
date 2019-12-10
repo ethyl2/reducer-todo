c4b8e000f70fd517fb2db245f65bd3cb21a7cced
Heather Nuffer <zzzuzz@gmail.com>
2019-12-10 12:20:44 -0700

    Added icon source to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <h3>Hunger Games Parody Songs</h3>
-            <a href="https://youtu.be/JXO44xIvyuY">Peeta's Song</a>
-            <a href="https://youtu.be/am1tzttZ8Pc">Katniss' Song</a>
-            <a href="https://youtu.be/1cayQGVTwjs">Gale's Song</a>
+            <div>
+                <h3>Hunger Games Parody Songs</h3>
+                <a href="https://youtu.be/JXO44xIvyuY">Peeta's Song</a>
+                <a href="https://youtu.be/am1tzttZ8Pc">Katniss' Song</a>
+                <a href="https://youtu.be/1cayQGVTwjs">Gale's Song</a>
+                <hr />
+            </div>
+            <a href='https://www.stockio.com/free-icon/nature-icons-flame'>Icon Source</a>
         </footer>
     )
 }
